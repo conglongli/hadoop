002790376d20046672e991869a433fcf6e473a56
conglongli <conglong.li@gmail.com>
2015-12-10 16:47:35 -0500

    logging

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -952,6 +952,8 @@ public class DFSInputStream extends FSInputStream
     ReaderStrategy byteArrayReader = new ByteArrayStrategy(buf);
     try (TraceScope ignored =
              dfsClient.newPathTraceScope("DFSInputStream#byteArrayRead", src)) {
+      DFSClient.LOG.info("Conglong Read Est 955 DFSInputStream Starting read blockId {} length {} from datanode {}",
+            getCurrentBlock().getBlockId(), getCurrentBlock().getNumBytes(), currentNode.getHostName());
       return readWithStrategy(byteArrayReader, off, len);
     }
   }
@@ -961,6 +963,8 @@ public class DFSInputStream extends FSInputStream
     ReaderStrategy byteBufferReader = new ByteBufferStrategy(buf);
     try (TraceScope ignored =
              dfsClient.newPathTraceScope("DFSInputStream#byteBufferRead", src)){
+      DFSClient.LOG.info("Conglong Read Est 966 DFSInputStream Starting read blockId {} length {} from datanode {}",
+            getCurrentBlock().getBlockId(), getCurrentBlock().getNumBytes(), currentNode.getHostName());
       return readWithStrategy(byteBufferReader, 0, buf.remaining());
     }
   }
