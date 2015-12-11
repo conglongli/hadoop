1e70dd84f99f9af25803927772d909975d7319ff
conglongli <conglong.li@gmail.com>
2015-12-11 06:45:49 -0500

    logging

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockSender.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockSender.java
@@ -738,6 +738,8 @@ class BlockSender implements java.io.Closeable {
     manageOsCache();
 
     final long startTime = ClientTraceLog.isDebugEnabled() ? System.nanoTime() : 0;
+    LOG.info("Conglong Read Act 741 BlockSender Start Sending blockId {} length {} from {}",
+          block.getBlockId(), block.getNumBytes(), datanode.getHostName());
     try {
       int maxChunksPerPacket;
       int pktBufSize = PacketHeader.PKT_MAX_HEADER_LEN;
